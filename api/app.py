3ef49ed46732d394fe75648f21740ed30670ea72
Chen Ning <chenningg97@gmail.com>
2020-11-22 01:11:18 +0800

    Added flask

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/time")
+def get_current_time():
+    return {"time": time.time()}
