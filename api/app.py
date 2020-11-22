ece65c26bd2df6cb77d78c14372da898d280c4fc
Chen Ning <chenningg97@gmail.com>
2020-11-22 16:28:44 +0800

    Interfaced frontend and backend to parse SQL

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from os.path import dirname, abspath
+from subprocess import call
 import time
 
 app = Flask(__name__)
@@ -9,6 +11,16 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/time")
-def get_current_time():
-    return {"time": time.time()}
+@app.route("/test", methods=["POST"])
+def get_plans():
+    # Gets the SQL query from the frontend
+    data = request.json
+    query = data["query"]
+
+    # Absolute filepath to run Picasso command line utility from
+    filepath = (
+        dirname(dirname(abspath(__file__)))
+        + "\picasso2.1\PicassoRun\Windows\PicassoCmd"
+    )
+
+    return query
--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,24 +1,41 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import axios from "axios";
 
 const FormInput = () => {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [query, setQuery] = useState("");
+  const [output, setOutput] = useState("");
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setOutput("Generating output...")
+
+    axios.post("/test", {
+      query: query,
+    })
+    .then((response) => {
+      setOutput(response.data);
+    })
+    .catch((error) => {
+      setOutput(error);
+    })
+  }
 
   return (
     <div className="flex">
       <div className="flex-item">
-        <h2>SQL query</h2>
-        <textarea placeholder="Input SQL query..."></textarea>
-        <button>Generate</button>
+        <form onSubmit={handleSubmit}>
+          <h2>SQL query</h2>
+          <textarea placeholder="Input SQL query..." onChange={handleChange} value={query}></textarea>
+          <button type="submit">Generate</button>
+        </form>
       </div>
       <div className="flex-item">
         <h2>Output</h2>
-        <textarea readOnly></textarea>
+        <textarea readOnly value={output}></textarea>
         <button>Toggle</button>
       </div>
     </div>)
