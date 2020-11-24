9ddd1098ecd4add097e058e4340e528f58a28d76
Chen Ning <chenningg97@gmail.com>
2020-11-24 22:23:06 +0800

    Created postorder query tree traversal

--- api/app.py
+++ api/app.py
@@ -33,9 +33,13 @@ def get_plans():
     data = json.dumps(ast.literal_eval(str(data)))
     # print("data: ", data, file=stderr)
 
-    postorder_qep(data)
+    optimized_query_plan = postorder_qep(data)
+    data = {"optimizedQueryPlan": optimized_query_plan}
 
-    return json.loads(data)
+    return data
+
+
+name = 65
 
 
 def postorder_qep(plan):
@@ -46,22 +50,82 @@ def postorder_qep(plan):
 
         postorder_result = []
 
-        # Output name of intermediate output
-        name = 65
+        global name
 
         def recurse(plan):
-            if not plan:
+            if plan == None:
                 return
 
-            # Recurse all the way down the tree and get the intermediate output(s)
-            if plan.get("Plans"):
-                for branch in plan.get("Plans"):
-                    postorder_result.append(recurse(branch), file=stderr)
+            global name
+
+            child_names = []
 
             # My own output name (e.g. A)
-            curr_name = chr(name)
+            curr_name = ""
+
+            # Recurse all the way down the tree and get the intermediate output(s)
+            if plan.get("Plans") != None:
+                for branch in plan.get("Plans"):
+                    result = recurse(branch)
+
+                    if result:
+                        child_names.append(result[0])
+                        postorder_result.append(result[1])
+
+                    # If not leaf node, give an arbitrary name
+                    curr_name = chr(name)
+                    name += 1
+            # If we are a leaf node, then just put table name
+            else:
+                curr_name = plan.get("Relation Name")
 
             # When we reach here, we can assume we are a leaf node, or that all our children has already recursed.
+            output = ""
+            node_type = plan.get("Node Type")
+            output += node_type + " "
+
+            # Take care of joins and sorts
+            if (
+                node_type == "Hash"
+                or node_type == "Sort"
+                or node_type == "Gather Merge"
+                or node_type == "Merge"
+                or node_type == "Aggregate"
+            ):
+                output += child_names[0] + " as " + curr_name + "."
+            elif (
+                node_type == "Hash Join"
+                or node_type == "Nested Loop"
+                or node_type == "Merge Join"
+            ):
+                if node_type == "Nested Loop":
+                    output += "Join "
+                output += (
+                    "between "
+                    + child_names[0]
+                    + " (outer) and "
+                    + child_names[1]
+                    + " (inner) as "
+                    + curr_name
+                    + "."
+                )
+            else:
+                output += "on " + curr_name + "."
+
+            print([curr_name, output], file=stderr)
+
+            return [curr_name, output]
+
+        output = ""
+        node_type = plan.get("Node Type")
+        result = recurse(plan)
+
+        if result:
+            output += node_type + " " + result[0] + " to get final result " + chr(name)
+
+        postorder_result.append(output)
+
+        return postorder_result
 
     except:
         print("Can't find any nodes in query execution plan.", file=stderr)
