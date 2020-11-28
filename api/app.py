82d0e0ced14fafe02250153457f895249c8a70be
Chen Ning <chenningg97@gmail.com>
2020-11-28 22:52:13 +0800

    Added query visualizer

--- api/app.py
+++ api/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
 
-from constant.constants import var_prefix_to_table, equality_comparators, range_comparators
+from constant.constants import (
+    var_prefix_to_table,
+    equality_comparators,
+    range_comparators,
+)
 
 from sys import stderr
 import ast
 import numpy as np
+
 # import sqlparse
 import string
 import re
@@ -16,10 +21,12 @@ from database_query_helper import *
 from generate_predicate_varies_values import *
 from postorder_qep import *
 from sqlparser import *
+from query_visualizer import *
 
 
 # Load environment variables
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Load Flask config
@@ -35,6 +42,8 @@ def hello():
 """ #################################################################### 
 used to generate a query plan based on the provided query
 #################################################################### """
+
+
 @app.route("/generate", methods=["POST"])
 def get_plans():
     # Gets the request data from the frontend
@@ -42,18 +51,19 @@ def get_plans():
 
     print(request_data, file=stderr)
 
-
     # Gets the query execution plan (qep) recommended by postgres for this query
-    qep_sql_string = "EXPLAIN (FORMAT JSON, BUFFERS) " + request_data["query"]
-
+    qep_sql_string = (
+        "EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) "
+        + request_data["query"]
+    )
 
     # Get the optimal qep
     optimal_qep = query(qep_sql_string, explain=True)
     optimal_qep = json.dumps(ast.literal_eval(str(optimal_qep)))
 
     explanation = postorder_qep(optimal_qep)
+    visualize_query(optimal_qep)
     optimal_qep = json.loads(optimal_qep)
-
 
     # Get the selectivity variation of this qep.
     if len(request_data["predicates"]) != 0:
@@ -66,7 +76,7 @@ def get_plans():
 Calculates the specific selectivities of each predicate in the query.
 #################################################################### """
 
-# def histogram(): # should be made into a class for clarity 
+# def histogram(): # should be made into a class for clarity
 #     statement = "SELECT histogram_bounds FROM pg_stats WHERE tablename='{}' AND attname='{}';".format(
 #                 predicate_table, predicate_attribute
 #             )
@@ -75,7 +85,7 @@ Calculates the specific selectivities of each predicate in the query.
 #     stats = query(statement)
 
 
-# def most_common_value(): # should be made into a class for clarity 
+# def most_common_value(): # should be made into a class for clarity
 #     # Use most common values (MSV) to determine selectivity requirement
 #     statement = "SELECT null_frac, n_distinct, most_common_vals, most_common_freqs FROM pg_stats WHERE tablename='{}' AND attname='{}';".format(
 #         predicate_table, predicate_attribute
@@ -91,18 +101,18 @@ def get_selectivities(sql_string, predicates):
         sqlparser.parse_query(sql_string)
 
         for predicate in predicates:
-            relation = var_prefix_to_table[predicate.split('_')[0]]
-            
+            relation = var_prefix_to_table[predicate.split("_")[0]]
+
             conditions = sqlparser.comparison[predicate]
-                        
+
             if conditions[0][0] in equality_comparators:
                 # some_returned_json = most_common_value()
                 pass
             else:
                 some_returned_json = get_histogram(relation, predicate, conditions)
-        
+
         return some_returned_json
-            
+
     except:
         print("Error", file=stderr)
 
@@ -136,11 +146,3 @@ def get_selective_qep(sql_string, selectivities, predicates):
             print("No where clause", file=stderr)
     except:
         print("ERROR!", file=stderr)
-
-
-
-
-
-
-
-
