5efe3b0e176cc9385e73716e1359c536a97b40bc
Chen Ning <chenningg97@gmail.com>
2020-11-30 01:25:08 +0800

    Added comparison table

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,12 +1,16 @@
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
 import FormInput from '../FormInput/FormInput';
+
+import styles from "./App.module.css";
 
 const App = () => {
   return (
     <Container fluid="md">
-      <div className="text-center">
+      <div className={styles.headerWrapper}>
         <h1>SQL Query Optimizer</h1>
-        <p className="lead">Input an SQL query, and we'll show you the most optimized query plan.</p>
+        <p className="lead">Input an SQL query to compare various execution plans.</p>
+        <p className={styles.headerText}>Welcome! Enter a properly formatted SQL query on the right. The query must be based on the <a href="http://www.tpc.org/tpch/" target="_blank" rel="noopener noreferrer">TPC-H</a> dataset, and requires a database set up according to <a href="https://github.com/chenningg/sql-query-optimizer#setting-up-postgresql-and-the-tpc-h-dataset" target="_blank" rel="noopener noreferrer">these</a> instructions. You can also select predicates to vary on the left, which will allow the DBMS (Postgresql) to vary the selectivity of those predicates to find alternate plans for comparison.</p>
+        <p className={styles.headerText}>Only allowable predicates have been added to the list, so don't be surprised if some attributes are missing! Allowable predicates include numeric and date attributes, and predicates can only be selected on attributes limited by a range condition in a WHERE clause (no equality conditions).</p>
       </div>
       <hr/>
       <FormInput/>
--- frontend/src/components/PlanComparison/PlanComparison.js
+++ frontend/src/components/PlanComparison/PlanComparison.js
@@ -7,23 +7,50 @@ const PlanComparison = (props) => {
   // Loads data for comparison.
   const getData = () => {
     if (props.output["error"] === false && props.output["data"].hasOwnProperty(props.planId)) {
-      return (
-        <Table striped bordered hover className={styles.comparisonTableWrapper}>
-          <thead>
-            <tr>
-              <th>Testing</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+      const planData = props.output["data"][props.planId];
+
+      // Load in selectivities, checking for range attributes    
+      const loadStats = planData["predicate_selectivity_data"].map((predicate, index) => {
+        let selectivity;
+        let value;
+
+        const attribute = predicate["attribute"];
+        const operator = predicate["operator"];
+
+        if (predicate["new_selectivity"] === null) {
+          selectivity = parseFloat(predicate["queried_selectivity"]).toFixed(3);
+          value = predicate["queried_value"];
+        }
+        else {
+          selectivity = parseFloat(predicate["new_selectivity"]).toFixed(3);
+          value = predicate["new_value"];
+        }
+
+        return (
+          <>
+            <tr key={`${index}-attribute`} className={styles.attributeTableCell}>
+              <td colspan="2"><b>{attribute}</b></td>
             </tr>
-          </thead>
+            <tr key={`${index}-value`}>
+              <td><b>Value</b></td>
+              <td>{`${operator} ${value}`}</td>
+            </tr>
+            <tr key={`${index}-selectivity`}>
+               <td><b>Selectivity</b></td>
+              <td>{selectivity}</td>
+            </tr>
+          </>
+        )
+      })      
+
+      return (
+        <Table bordered hover responsive className={styles.comparisonTableWrapper}>
           <tbody>
             <tr>
-              <td>Test</td>
-              <td>Test</td>
-              <td>Test</td>
-              <td>Test</td>
+              <td><b>Estimated cost per row</b></td>
+              <td>{parseFloat(planData["estimated_cost_per_row"]).toFixed(3)}</td>
             </tr>
+            {loadStats}
           </tbody>
         </Table>
       )
@@ -34,7 +61,7 @@ const PlanComparison = (props) => {
   }
 
   return (
-    getData() !== null ?
+    props.output["data"].hasOwnProperty(props.planId) ?
     <div>
       {getData()}
     </div> :
